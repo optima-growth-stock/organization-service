e3dd5a5051422dbe2fdcc349c2d31bbe93b35f99
Toni Vrdoljak <toni.vrdoljak4@gmail.com>
2022-07-02 13:36:10 +0200

    Change versions

--- src/main/java/com/optimagrowth/organization/OrganizationServiceApplication.java
+++ src/main/java/com/optimagrowth/organization/OrganizationServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
-@RefreshScope
 public class OrganizationServiceApplication {
 
 	public static void main(String[] args) {
